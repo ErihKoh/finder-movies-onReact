a1d81c49841245da3fddde6df638b85cdcff2d14
Yurii Zubar <barsuk0831@gmail.com>
2020-12-24 22:53:11 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import AppBar from './components/AppBar/AppBar';
+import Container from './components/Container/Container';
+import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import MovieDetailsPage from './views/MovieDetailsPage';
+// import NotFoundView from './views/NotFoundView';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <AppBar />
+
+      {/* <Switch> */}
+      {/* <Route path="/" exact>
+          <HomePage />
+        </Route>
+
+        <Route path="/movies">
+          <MoviesPage />
+        </Route> */}
+
+      {/* <Route path="">
+          <MovieDetailsPage />
+        </Route>
+
+        <Route>
+          <NotFoundView />
+        </Route> */}
+      {/* </Switch> */}
+    </Container>
   );
 }
-
-export default App;
